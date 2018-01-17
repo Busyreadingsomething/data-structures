1df39be4409693592212428ed3f0aa93c727f4cd
Busyreadingsomething <oro1919@gmail.com>
2018-01-17 15:55:02 -0800

    Finished functional shared for Queue and Stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,12 +1,59 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {};
-  var storageMethods = {};
+  // create instance object
+  var stackInstance = {};
+  // var storage = {};
+  stackInstance.storage = {};
+  // stackInstance.enqueue = queueMethods.enqueue;
+  // stackInstance.dequeue = queueMethods.dequeue;
+  // stackInstance.size = queueMethods.size;
+  // extend methods from shared method object
+  _.extend(stackInstance, queueMethods);
+  // return instance object
+  return stackInstance;
 
 };
 
-var queueMethods = {}
+var queueMethods = { 
+  // create enqueue method
+  enqueue: function (value) {
+  // debugger;
+    var key = this.size();
+    this.storage[key] = value;
+  },
+  //takes in parameter 'value'
+    // call size method to obtain size of storage
+    // use size as key to storage method
+  // create dequeue method
+  dequeue: function () {
+    var holder = [];
+    for (var key in this.storage) {
+      holder.push(this.storage[key]);
+      // holder[holder.length] = this.storage[key]
+    }
+    this.storage = {};
+    for (var i = 1; i < holder.length; i++) {
+      this.storage[i - 1] = holder[i];
+    }
+    return holder[0];
+  }, 
+    //declare a holder array
+    //loop through storage object
+      //push to holder the value
+    //reset storage object
+    // loop through holder array, starting at index 1
+      //set storage object key to index-1
+      // set value of storage object to holder[i] 
+    // return holder[0]
+  // create size method
+  size: function () {
+    return Object.keys(this.storage).length;
+  }
+    // find list of keys inside storage object
+    //return length of list
+};
+
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,9 +1,52 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {};
+  
+  // create stackInstance obj
+  var stackInstance = {};
+  // create len prop in sI
+  stackInstance.len = 0;
+  // create storage prop in sI
+  stackInstance.storage = {};
+  // invoke extend passing sI and sM
+  _.extend(stackInstance, stackMethods);
+  // return sI
+  return stackInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  //create push method
+  push: function (value) {
+    var key = this.len;
+    this.storage[key] = value;
+    this.len++;
+  },
+  // takes prarameter value
+    // call the len prop
+    // set storage[len] = value
+    // increase len by one
+  //create pop method
+  pop: function () {
+    var key = this.len - 1;
+    var deleted = this.storage[key];
+    if (this.len !== 0) {
+      this.len--;
+    }
+    delete this.storage[key];
+    return deleted;
+  },
+    // call len prop
+    // set variable to len prop - 1
+    // find at storage[variable] and set to variable
+    // delete storage [variable]
+    // if len not zero
+      // decrease len prop by one
+    // return variable with storage[var] value
+  // create size method
+  size: function () {
+    return this.len;
+    // return this len
+  }
+};
 
 

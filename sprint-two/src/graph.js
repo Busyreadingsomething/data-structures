6c0a8798f54ade52524eb493ccc6615e23ec58de
Busyreadingsomething <oro1919@gmail.com>
2018-01-19 16:31:47 -0800

    completed resizing hashtable, created HTML stress, cleaned up graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -7,72 +7,53 @@ var Graph = function() {
   // create object prototype for graph
   // create this.value
   // create edge 
-  graph.nodes = [];
-  graph.edge = [];
+  graph.nodes = {};
+  graph.edge = {};
   // return object created
   return graph;
 };
 
 // Add a node to the graph, passing in the node's IDENTIFIER.
 Graph.prototype.addNode = function(node) {
-  this.nodes.push(node);
-  this.edge[node] = [];
+  this.nodes[node] = node;
+  this.edge[node] = {};
 };
 
 // Return a boolean value indicating if the IDENTIFIER passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  for (var i = 0; i < this.nodes.length; i++) {
-    if (node === this.nodes[i]) {
-      return true;
-    }
-  }
-  return false;
+  return node in this.nodes;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  // debugger;
-  for (var i = 0; i < this.nodes.length; i++) {
-    var connection = this.nodes[i];
-    if (node === connection) {
-      this.nodes.splice(i, 1);
-    } else if (this.hasEdge(node, connection)) {
-      var loc = this.edge[connection].indexOf(node);
-      this.edge[connection].splice(loc, 1);
-    }
+  delete this.nodes[node];
+  delete this.edge[node];
+  for (var key in this.edge) {
+    delete this.edge[key][node];
   }
-  this.edge[node].splice(0);
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  return (this.edge[fromNode].includes(toNode) || this.edge[toNode].includes(fromNode));
+  return toNode in this.edge[fromNode] && fromNode in this.edge[toNode];
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.edge[fromNode].push(toNode);
-  this.edge[toNode].push(fromNode);
+  this.edge[fromNode][toNode] = toNode;
+  this.edge[toNode][fromNode] = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  for (var i = 0; i < this.edge[fromNode].length; i++) {
-    if(toNode === this.edge[fromNode][i]) {
-      this.edge[fromNode].splice(i, 1);
-    }
-  }
-  for (var i = 0; i < this.edge[toNode].length; i++) {
-    if(fromNode === this.edge[toNode][i]) {
-      this.edge[toNode].splice(i, 1);
-    }
-  }
+  delete this.edge[toNode][fromNode];
+  delete this.edge[fromNode][toNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  for (var i = 0; i < this.nodes.length; i++) {
-    cb(this.nodes[i]);
+  for (var key in this.nodes) {
+    cb(this.nodes[key]);
   }
   // loop through this nodes
     // invoke cb on nodes[i];
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,6 +3,7 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -23,6 +24,10 @@ HashTable.prototype.insert = function(k, v) {
   if (!itExists) {
     bucket.push(tuple);
   }
+  this._count++;
+  if (this.ratio() > 0.75) {
+    this.resize();
+  }
   // var tuple is k with v
   // if storage at index is undefined
     // set storage at index to []
@@ -35,9 +40,11 @@ HashTable.prototype.insert = function(k, v) {
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      return bucket[i][1];
+  if (bucket !== undefined) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
     }
   }
   // set bucket variable to this get at index
@@ -48,13 +55,19 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
-  debugger;
+  // debugger
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      bucket.splice(i, 1);
+  if (bucket !== undefined) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket.splice(i, 1);
+      }
     }
+    this._count--;
+  }
+  if (this.ratio() < 0.25) {
+    this.resize();
   }
   // set bucket to storage get at index
   // loop through bucket
@@ -64,7 +77,43 @@ HashTable.prototype.remove = function(k) {
   // console.log('remove: ' + index, this._storage);
 };
 
+HashTable.prototype.ratio = function() {
+  return this._count / this._limit;
+};
 
+HashTable.prototype.resize = function() {
+  var holder = [];
+
+  for (var i = 0; i < this._limit; i++) {
+    var bucket = this._storage.get(i);
+    if (bucket !== undefined) {
+      for (var j = 0; j < bucket.length; j++) {
+        holder.push(bucket[j]);
+      }
+    }
+  }
+
+  if (this.ratio() > 0.75) {
+    this._limit = this._limit * 2;
+  } else if (this.ratio() < 0.25) {
+    this._limit = Math.floor(this._limit / 2);
+  }
+  
+  this._storage = LimitedArray(this._limit);
+
+  this._count = 0;
+  for (var i = 0; i < holder.length; i++) {
+    this.insert(holder[i][0], holder[i][1]);
+  }
+  // create a holder to store all tuples
+  // create for loop for storage using counts
+  // create if this.ratio > 0.75 then double
+    //
+  // else if this ratio < 0.25 then halve
+    //
+  // reset count to 0
+  //create loop for inserting
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
